c6cbe26e6824510a6bb43f1b886f7df8c8a67322
Ayman Karram <ayman.karram@asanarebel.com>
2020-02-01 21:31:58 +0100

    Created URLComponentsExtensionTests

---
+++ PopularGithubRepositoriesTests/Network/URLComponentsExtensionTests.swift
@@ -0,0 +1,55 @@
+//
+//  URLComponentsExtensionTests.swift
+//  PopularGithubRepositoriesTests
+//
+//  Created by Ayman Karram on 01.02.20.
+//  Copyright © 2020 Ayman Karram. All rights reserved.
+//
+
+import XCTest
+@testable import PopularGithubRepositories
+
+class URLComponentsExtensionTests: XCTestCase {
+    func testInit() {
+        let parameters = ["MockParamter": "value"]
+        let mockService = MockService(paramters: parameters)
+        let urlComponents = URLComponents(service: mockService)
+        XCTAssertEqual(urlComponents.queryItems?.first?.name, parameters.keys.first)
+        XCTAssertEqual(urlComponents.queryItems?.first?.value, parameters.values.first)
+        let urlStringWithParmaters = "https://MockService/ios?MockParamter=value"
+        XCTAssertEqual(urlComponents.url?.absoluteString, urlStringWithParmaters)
+    }
+}
+
+final class MockService: ServiceProtocol {
+
+    var paramters: Parameters?
+
+    init(paramters: Parameters?) {
+        self.paramters = paramters
+    }
+
+    var baseURL: URL {
+        return URL(string: "https://MockService")!
+    }
+
+    var path: String {
+        return "ios"
+    }
+
+    var method: HTTPMethod {
+        return .get
+    }
+
+    var task: Task {
+        return .requestParameters(self.paramters ?? [:])
+    }
+
+    var headers: RequestHeaders? {
+        return nil
+    }
+
+    var parametersEncoding: ParametersEncoding {
+        return .url
+    }
+}
